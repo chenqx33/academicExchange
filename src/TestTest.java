c58e49e3e2596844028da01c781439cb429ef385
chenqx <chenqx@fsxiaoke.com>
2018-07-20 17:55:27 +0800

    注解

--- src/TestTest.java
+++ src/TestTest.java
@@ -1,5 +1,13 @@
-import java.util.Arrays;
+import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import pojo.MyAbst;
 import pojo.MyImpl;
 import pojo.MyIntegerface;
 
@@ -14,4 +22,42 @@ public class TestTest {
         MyIntegerface<String> impl = new MyImpl();
         impl.printAll(Arrays.asList("1","2","3"));
     }
+
+    //codeReview
+    @Test
+    public void codeReview(){
+        String a = "test";
+        List<String> test = Collections.singletonList(a);
+        //不可修改
+        test.add("testhaha");
+    }
+
+    @Test
+    public void codeReview2(){
+        String a = "test";
+        Set<String> test = Collections.singleton(a);
+        //不可修改
+        test.add("testhaha");
+    }
+
+    @Test
+    public void initList(){
+        List<String> list = new ArrayList<String>(){
+            @Override
+            public boolean add(String s) {
+                return false;
+            }
+        };
+        System.out.println(list.add("12"));
+    }
+
+    @Test
+    public void my(){
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+        System.out.println(sdf.format(1532066434399l));
+    }
+
+
+
+
 }
---
+++ src/pojo/annotataion/UserCaseTest.java
@@ -0,0 +1,32 @@
+package pojo.annotataion;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author cqx
+ * @date 2018/7/20 17:47
+ */
+public class UserCaseTest {
+    public static void main(String[] args) {
+        List<Integer> userCasses = new ArrayList<>();
+        Collections.addAll(userCasses,47,48,49,50);
+        trackUseCases(userCasses,PassWordUtils.class);
+    }
+    public static void trackUseCases(List<Integer> useCases,Class<?> c1){
+        for (Method m : c1.getDeclaredMethods()) {
+            //获得注解的对象
+            UserCase.UserCases uc = m.getAnnotation(UserCase.UserCases.class);
+            if (uc != null) {
+                System.out.println("Found Use Case:" + uc.id() + " "
+                        + uc.description());
+                useCases.remove(new Integer(uc.id()));
+            }
+        }
+        for (int i : useCases) {
+            System.out.println("Warning: Missing use case-" + i);
+        }
+    }
+}
