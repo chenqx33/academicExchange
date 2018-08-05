698c85cc963fbd123ed885f60f7b3b7582d70461
nigul <chenqx33@163.com>
2018-08-05 21:08:31 +0800

    maven

---
+++ comchenqx/src/main/java/com/chenqx/Test.java
@@ -0,0 +1,9 @@
+package com.chenqx;
+
+import lombok.Data;
+
+@Data
+public class Test {
+    private Integer i;
+
+}
---
+++ comchenqx/src/test/java/TestTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestTest {
+    @Test
+    public void getI() throws Exception {
+    }
+
+    @Test
+    public void setI() throws Exception {
+    }
+
+}
